61366872131a129d7b120cd30887a710e84da73a
Mandar_D <mdman2257@gmail.com>
2026-05-07 15:41:26 +0530

    initial expo react native app setup

---
+++ rn_frontend/App.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import './global.css'
+
+
+export default function App() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-red-400'>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
